ded544fd8ff8be3553bf60344a7a4cd0d20d5c0e
SudiptoSarkar123 <sudiptosarkar833@gmail.com>
2024-10-17 13:01:46 +0530

    inital commit 2

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function typeText() {
 window.onload = function() {
     typeText();
 };
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    const audio = document.getElementById('backgroundMusic');
+    const radioToggle = document.getElementById('radioToggle');
+    const radioIconContainer = document.querySelector('.radio-icon');
+
+    // Auto-play music when the page loads
+    audio.play();
+
+    // Toggle music on/off when radio icon is clicked
+    radioToggle.addEventListener('click', function() {
+        if (audio.paused) {
+            audio.play();
+            radioIconContainer.classList.remove('paused'); // Remove grayscale when playing
+            radioToggle.title = "Click to stop music";
+        } else {
+            audio.pause();
+            radioIconContainer.classList.add('paused'); // Add grayscale when paused
+            radioToggle.title = "Click to play music";
+        }
+    });
+});
